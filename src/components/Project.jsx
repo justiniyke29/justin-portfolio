118aa0d0cdb4573e8048fddeebda377123201a88
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-10-15 21:20:24 +0100

    new

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,10 +4,10 @@ import proimg2 from "../assets/Project-1.png"
 
 const Project = () => {
   return (
-    <div className='w-full h-screen text-gray-300'>
+    <div className='w-full h-screen bg-[#0a192f] text-gray-300'>
     <div className='flex flex-col justify-center items-center w-full h-full'>
       <a href='https://justiniyke-shoppingmall.vercel.app' target="_blank" rel="noreferrer">
-      <img  className='w-60 mx-auto' src={proimg} alt='projectimage' />
+      <img  className='w-400 h-300' src={proimg} alt='projectimage' />
       </a>
     </div>
     <div className='flex flex-col justify-center items-center w-full h-full'>
