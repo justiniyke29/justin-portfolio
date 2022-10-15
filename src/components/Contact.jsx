52c0a721ae03f78502a612a71f73c4fe39d7ae2d
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-10-15 21:46:09 +0100

    adjusted margin top in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/f54ab410-de2e-480d-958f-ff1c340a74c0" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 mt-2.5' >Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - justiniyke1995@gmail.com</p>
             </div>
             <input className='bg-[#ccd6f6] p-2' type="text" placeholder='Name' name='name' />
