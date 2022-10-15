60804aab1edc1bb3b542026d04776fe5bfd8be58
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-10-15 21:58:10 +0100

    ss

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,11 +7,13 @@ const Project = () => {
     <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
               Project
       </p>
-    <div className='flex flex-col justify-center items-center w-full h-full mb-5.5' >
+    <div className='flex flex-col justify-center items-center w-full h-full ' >
     <p className='text-gray-300 py-4'>Ecommerce website</p>
       <a href='https://justiniyke-shoppingmall.vercel.app' target="_blank" rel="noreferrer">
       <img  className='w-auto mx-auto' src={proimg} alt='projectimage' />
       </a>
+      <br/>
+      <br />
     </div>
     </div>
   )
