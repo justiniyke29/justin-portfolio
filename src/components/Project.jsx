01b2a63b17de0227d4f39d8fd91c0c28c24c3a6a
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-10-15 20:11:22 +0100

    2

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import proimg from "../assets/project-2.png"
 
 const Project = () => {
   return (
     <div className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      <a href='https://justiniyke-shoppingmall.vercel.app'>
-      <img className='project-img' src='../assets/project-2' alt='projectimage' />
+      <a href='https://justiniyke-shoppingmall.vercel.app' target="_blank">
+      <img className='project-img' src={proimg} alt='projectimage' />
       </a>
     </div>
   )
