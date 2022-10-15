b994abd2a5f1a066f9d68a11b63aa959708f03cd
justiniyke29 <106402840+justiniyke29@users.noreply.github.com>
2022-10-15 21:31:42 +0100

    hh

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import proimg from "../assets/project-2.png"
-import proimg2 from "../assets/Project-1.png"
 
 const Project = () => {
   return (
     <div className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              Project
+      </p>
     <div className='flex flex-col justify-center items-center w-full h-full'>
       <a href='https://justiniyke-shoppingmall.vercel.app' target="_blank" rel="noreferrer">
       <img  className='w-400 h-300' src={proimg} alt='projectimage' />
       </a>
     </div>
-    <div className='flex flex-col justify-center items-center w-full h-full'>
-      <a href='https://justiniyke-shoppingmall.vercel.app' target="_blank" rel="noreferrer">
-      <img  className='w-400 h-300' src={proimg2} alt='projectimage22' />
-      </a>
-    </div>
+    <p className='text-gray-300 py-4'>Ecommerce website</p>
     </div>
   )
 }
